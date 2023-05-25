55b1e8acfcc59b9a32b4cc17b55fa6e039d6b845
mldkyt <mldkyt@proton.me>
2023-05-25 15:59:37 +0200

    Meta tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,15 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* urban grocery store title and short description, color brown */}
+        <meta name="theme-color" content="#795548" />
+        <meta name='description' content='Urban Grocery Store' />
+        <meta name='keywords' content='urban, grocery, store' />
+        <meta name='author' content='mldkyt, TanZan' />
+        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+        <meta name='og:title' content='Urban Grocery Store' />
+      </Head>
       <body>
         <Main />
         <NextScript />
