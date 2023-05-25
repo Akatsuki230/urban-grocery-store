251376115302ab1b79e08ee0b2120994e55f8b26
mldkyt <mldkyt@proton.me>
2023-05-25 16:24:36 +0200

    Fix a bug in ordering system

--- src/pages/api/order.ts
+++ src/pages/api/order.ts
@@ -4,7 +4,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     const { username, items } = JSON.parse(req.body);
     let message = `User order from "${username}":\n`;
     for (const item in items) {
-        message += `\t${item + 1}: ${items[item]}\n`
+        const readableIndex = parseInt(item) + 1;
+        message += `\t${readableIndex}: ${items[item]}\n`
     }
     const webhook = 'https://discord.com/api/webhooks/1111284665198837874/6pngT1eND80PkNOpIkR8dveDwsGoraBkQHWmrPQZHeWe1qkpwdS0GiCdH3u-AQ30O0Wl'
     fetch(webhook, {
